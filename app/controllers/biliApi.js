d4c077605d97345eb3b4ed921f12b3badbfaf3ff
ling <kuse@qq.com>
2016-05-14 20:58:56 +0800

    小细节错误o(╯□╰)o

--- app/controllers/biliApi.js
+++ app/controllers/biliApi.js
@@ -160,7 +160,7 @@ exports.bilidown = function (req,res) {
 exports.bilidamku = function (req,res) {
   res.json({1:1})
   Movie
-    .find({up_uploadtime:{$gte:'2016-05-01 00:00',$lt:'2016-05-05 24:00'}},{xml:true})
+    .find({up_uploadtime:{$gte:'2016-05-01 00:00',$lt:'2016-05-05 24:00'},xml:true})
     .limit(20)
     .exec(function(err,movies){
       if(err) console.log(err);
@@ -172,7 +172,7 @@ exports.bilidamku = function (req,res) {
         var parser = new xml2js.Parser()
         fs.readFile(oxnp,function(err,data){
           if(data) {
-            console.log('success: '+mid);
+            console.log('success: ' + mid);
             parser.parseString(data,function(err,result){
               if(err) console.log(err)
               else if(result && result.i.d){
